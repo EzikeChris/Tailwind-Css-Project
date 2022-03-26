6ae3047a0e6b47e93569adf839bc5c92f0e6ba2a
EzikeChris <christophermbe1009@gmail.com>
2022-03-27 00:41:29 +0100

    1 file modified

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './output.css';
 
 export default function App() {
   return (
+    /*Create Custome tailwind css theme */
     <div className="theme-light">
       <h1 className="text-primary">Hello!</h1>
       <p>Start editing to see some magic happen</p>
